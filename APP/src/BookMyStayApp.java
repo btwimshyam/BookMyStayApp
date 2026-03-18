ea198398b5730878a94c7d8c150e90196c820b17
yashrajsharan18 <yashraj10messi@gmail.com>
2026-03-18 09:23:32 +0530

    Room Search

--- APP/src/BookMyStayApp.java
+++ APP/src/BookMyStayApp.java
@@ -1,85 +1,33 @@
-abstract class Room {
-    int beds;
-    int size;
-    double price;
+class Room {
+    int roomId;
+    String type;
+    boolean isAvailable;
 
-    Room(int beds, int size, double price) {
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
-    }
-
-    abstract void displayRoomDetails();
-}
-
-class SingleRoom extends Room {
-
-    SingleRoom() {
-        super(1, 200, 3000);
-    }
-
-    void displayRoomDetails() {
-        System.out.println("Room Type: Single Room");
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqft");
-        System.out.println("Price: ₹" + price);
+    Room(int roomId, String type, boolean isAvailable) {
+        this.roomId = roomId;
+        this.type = type;
+        this.isAvailable = isAvailable;
     }
 }
 
-class DoubleRoom extends Room {
-
-    DoubleRoom() {
-        super(2, 350, 5000);
-    }
-
-    void displayRoomDetails() {
-        System.out.println("Room Type: Double Room");
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqft");
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-class SuiteRoom extends Room {
-
-    SuiteRoom() {
-        super(3, 500, 9000);
-    }
-
-    void displayRoomDetails() {
-        System.out.println("Room Type: Suite Room");
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqft");
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-public class BookMyStayApp{
-
+public class BookMyStayApp {
     public static void main(String[] args) {
 
-        // Static availability variables
-        int singleRoomAvailable = 10;
-        int doubleRoomAvailable = 7;
-        int suiteRoomAvailable = 3;
+        Room[] rooms = {
+                new Room(101, "Single", true),
+                new Room(102, "Double", false),
+                new Room(103, "Single", true),
+                new Room(104, "Suite", true)
+        };
 
-        // Room objects
-        Room single = new SingleRoom();
-        Room dbl = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        String searchType = "Single";
 
-        // Display details
-        System.out.println("=== Room Details ===");
+        System.out.println("Available " + searchType + " rooms:");
 
-        single.displayRoomDetails();
-        System.out.println("Available: " + singleRoomAvailable);
-        System.out.println();
-
-        dbl.displayRoomDetails();
-        System.out.println("Available: " + doubleRoomAvailable);
-        System.out.println();
-
-        suite.displayRoomDetails();
-        System.out.println("Available: " + suiteRoomAvailable);
+        for (Room r : rooms) {
+            if (r.type.equalsIgnoreCase(searchType) && r.isAvailable) {
+                System.out.println("Room ID: " + r.roomId);
+            }
+        }
     }
 }
